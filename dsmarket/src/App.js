a54cc5bbc8f195c1f40588bf2c62675e04592d7c
Sehoon <tpgns0223@dsm.hs.kr>
2020-11-25 09:24:11 +0900

    상품페이지/게시판 스타일링

--- dsmarket/src/App.js
+++ dsmarket/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import PostList from './components/PostListPage/PostList';
-import Product from './components/PostListPage/Product';
+
 
 function App() {
   return (
     <>
-      <PostList />
     </>
   );
 }
