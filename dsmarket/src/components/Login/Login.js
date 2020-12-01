ff29b93ffe701f542f022b4aaef91294c031b092
성동명 <sungdm0303@gmail.com>
2020-12-01 16:39:43 +0900

    login

--- dsmarket/src/components/Login/Login.js
+++ dsmarket/src/components/Login/Login.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import * as L from '../../assets/style/Login/LoginForm';
-import { logo2 } from '../../assets/img'
+import { logo2 } from '../../assets/img';
+import axios from 'axios';
+
+const onSubmitLogin = () => {
+    const id = document.getElementById('id').value;
+    const password = document.getElementById('password').value;
+    const data = {id, password};
+    axios.post('http://3.35.209.149/accounts/auth', data)
+    .then((res) => {
+        console.log(res);
+        localStorage.setItem('accessToken', res.data.accessToken);
+    }).catch((error) => {
+        console.log(error);
+    })
+}
 
 const Login = () => {
     return(
@@ -20,17 +34,17 @@ const Login = () => {
                     </L.centerAlignment>                    
                     <L.centerAlignment>
                         <L.container>
-                            <L.input placeholder="Email"></L.input>
+                            <L.input type='email' placeholder="Email" id='id'></L.input>
                         </L.container>
                     </L.centerAlignment>
                     <L.centerAlignment>
                         <L.container>
-                            <L.input placeholder="Password"></L.input>
+                            <L.input type='password' placeholder="Password" id='password'></L.input>
                         </L.container>
                     </L.centerAlignment>
                     <L.centerAlignment>
                         <L.container>
-                            <L.loginButton>로그인</L.loginButton>
+                            <L.loginButton onClick={onSubmitLogin}>로그인</L.loginButton>
                         </L.container>
                     </L.centerAlignment>
                     <L.centerAlignment>
